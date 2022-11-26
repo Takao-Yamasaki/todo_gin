8a918e5fb72102b63aa5776860c6dddb1fcf3bbe
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-11-26 21:16:11 +0900

    CRUD処理の実装

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	_ "text/tabwriter"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
@@ -92,11 +92,70 @@ func main() {
 	// HTMLを読み込むディレクトリを指定
 	router.LoadHTMLGlob("templates/*.html")
 
-	data := "Hello Go/Gin!!"
+	dbInit()
 
+	//Index
 	// index.htmlにGetで繋いでいる
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(200, "index.html", gin.H{"data": data})
+		todos := dbGetAll()
+		c.HTML(200, "index.html", gin.H{
+			"todos": todos,
+		})
+	})
+
+	//Create
+	router.POST("/new", func(c *gin.Context) {
+		text := c.PostForm("text")
+		status := c.PostForm("status")
+		dbCreate(text, status)
+		c.Redirect(302, "/")
+	})
+
+	//Detail
+	router.GET("/detail:id", func(c *gin.Context) {
+		n := c.Param("id")
+		// idの値をint型に変換
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic(err)
+		}
+		todo := dbGetOne(id)
+		c.HTML(200, "detail.html", gin.H{"todo": todo})
+	})
+
+	//Update
+	router.POST("/update/:id", func(c *gin.Context) {
+		n := c.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		text := c.PostForm("text")
+		status := c.PostForm("status")
+		dbUpdate(id, text, status)
+		c.Redirect(302, "/")
+	})
+
+	//削除確認
+	router.GET("/delete_check/:id", func(c *gin.Context) {
+		n := c.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		todo := dbGetOne(id)
+		c.HTML(200, "delete.html", gin.H{"todo:": todo})
+	})
+
+	//Delete
+	router.POST("/delete/:id", func(c *gin.Context) {
+		n := c.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		dbDelete(id)
+		c.Redirect(302, "/")
 	})
 
 	// ポートを指定していないので、デフォルトのポート(8080)で待受
