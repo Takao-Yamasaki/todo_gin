0618cd98795a9c7b7e51806730097380cfbb348e
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-11-26 17:27:20 +0900

    goで渡した変数をhtmlに渡す

--- main.go
+++ main.go
@@ -9,9 +9,11 @@ func main() {
 	// HTMLを読み込むディレクトリを指定
 	router.LoadHTMLGlob("templates/*.html")
 
+	data := "Hello Go/Gin!!"
+
 	// index.htmlにGetで繋いでいる
 	router.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(200, "index.html", gin.H{})
+		ctx.HTML(200, "index.html", gin.H{"data": data})
 	})
 
 	router.Run()
